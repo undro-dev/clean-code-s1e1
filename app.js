03ff5181ab6076629fdacd85292330d49d45f352
Vitaly Undro <vitalyundro@gmail.com>
2024-01-08 19:51:40 +0300

    fix: add semantic tags in index.html according to rule 1.1. in html-and-css-extended.md

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var createNewTaskElement = function (taskString) {
 	//input (checkbox)
 	var checkBox = document.createElement("input"); //checkbx
 	//label
-	var label = document.createElement("label"); //label
+	var label = document.createElement("p"); //label
 	//input (text)
 	var editInput = document.createElement("input"); //text
 
@@ -78,7 +78,7 @@ var editTask = function () {
 	var listItem = this.parentNode;
 
 	var editInput = listItem.querySelector("input[type=text]");
-	var label = listItem.querySelector("label");
+	var label = listItem.querySelector("p");
 	var editBtn = listItem.querySelector(".edit");
 	var containsClass = listItem.classList.contains("edit-mode");
 	//If class of the parent is .edit-mode
