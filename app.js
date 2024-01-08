674052ca0e729d472c543d4ed15e08e286887006
Vitaly Undro <vitalyundro@gmail.com>
2024-01-08 19:57:39 +0300

    fix: add alternative text (alt) in index.html according to rule 1..2. (HTML & CSS. Advanced level.)

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ var createNewTaskElement = function (taskString) {
 	var deleteButton = document.createElement("button"); //delete button
 	var deleteButtonImg = document.createElement("img");
 	deleteButtonImg.classList.add("delete-img"); //delete button image
+	deleteButtonImg.alt = "btn-delete";
 
 	label.innerText = taskString;
 	label.className = "task label-task";
